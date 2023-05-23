231424a747905bfaa433cc2af1e39237375ca9d3
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2023-05-23 21:25:55 +0530

    header is added with searchbox

--- src/components/header/Searchbox/searchbox.js
+++ src/components/header/Searchbox/searchbox.js
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./searchbox.css";
 
+function SearchBox(props){
+    const ref = useRef();
 
-function SearchBox(){
+    const { onClickOutside } = props;
+  
+    useEffect(() => {
+  
+      const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+          onClickOutside();
+        }
+      };
+  
+      document.addEventListener('click', handleClickOutside, true);
+      return () => {
+        document.removeEventListener('click', handleClickOutside, true);
+      };
+    }, [onClickOutside]);
+  
+    if(!props.show)
+      return null;
+  
     return(
-        <div id="search-screen" class="inactive">
-            <input type="text" class="search-box" id="search-box" name="search" autocomplete="off"/>
-            <ul class="recent-searches-sec">
-                <li class="searched-item">
-                    <span class="material-symbols-outlined">
-                        history
-                        </span>
-                    <label class="searched-item-text">Hello World</label>
-                    <button class="search-item-del">
-                        <span class="material-symbols-outlined">
-                            delete
-                            </span>
-                    </button>
+        <div id="search-screen" ref={ref}>
+            <input type="text" className="search-box" id="search-box" name="search" autoComplete="off" />
+            <ul className="recent-searches-sec">
+                <li className="searched-item">
+                <span className="material-symbols-outlined">
+                    history
+                </span>
+                <label className="searched-item-text">Hello World</label>
+                <button className="search-item-del">
+                    <span className="material-symbols-outlined">
+                    delete
+                    </span>
+                </button>
                 </li>
-                <li class="searched-item">
-                    <span class="material-symbols-outlined">
-                        history
-                        </span>
-                    <label class="searched-item-text">Aaj piya ghar</label>
-                    <button class="search-item-del">
-                        <span class="material-symbols-outlined">
-                            delete
-                            </span>
-                    </button>
+                <li className="searched-item">
+                <span className="material-symbols-outlined">
+                    history
+                </span>
+                <label className="searched-item-text">Aaj piya ghar</label>
+                <button className="search-item-del">
+                    <span className="material-symbols-outlined">
+                    delete
+                    </span>
+                </button>
                 </li>
             </ul>
         </div>
     )
-}
+  }
 
 export default SearchBox;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,44 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import "./header.css";
-import SearchBox from "./Searchbox/searchbox.js";
+import SearchBox from "./Searchbox/searchbox";
 
-function Header(){
-    return(
-        <header id="header-prime">
-            <nav>
-                <div class="site-logo"><p>Satwik</p></div>
-                <ul class="nav-items">
-                    <li class="item selected-item">
-                        <div onclick="redirec('#')">
-                            <span class="material-symbols-outlined">home</span><label>Home</label>
-                        </div>
-                    </li>
-                    <li class="item">
-                        <div onclick="redirec('explore.html')">
-                            <span class="material-symbols-outlined">explore</span><label>Explore</label>
-                        </div>
-                    </li>
-                    <li class="item">
-                        <div href="#">
-                            <span class="material-symbols-outlined">library_music</span><label>Library</label>
-                        </div>
-                    </li>
-                    <li class="item">
-                        <div href="#" id="search-sec">
-                            <span class="material-symbols-outlined">search</span><label>Search</label>
-                        </div>
-                    </li>
-                </ul>
-                <div class="account-sec">
-                    <span class="material-symbols-outlined">
-                        account_circle
-                        </span>
-                </div>
-                
-            </nav>
-            
-        </header>
-    )
+function Header() {
+
+  const [openSearch, setOpenSearch] = useState(false);
+  
+  return (
+    <>
+      <header id="header-prime">
+        <nav>
+          <div className="site-logo">
+            <p>Satwik</p>
+          </div>
+          <ul className="nav-items">
+            <li className="item selected-item">
+              <div onClick="redirec('#')">
+                <span className="material-symbols-outlined">home</span>
+                <label>Home</label>
+              </div>
+            </li>
+            <li className="item">
+              <div onClick="redirect('explore.html')">
+                <span className="material-symbols-outlined">explore</span>
+                <label>Explore</label>
+              </div>
+            </li>
+            <li className="item">
+              <div href="#">
+                <span className="material-symbols-outlined">library_music</span>
+                <label>Library</label>
+              </div>
+            </li>
+            <li className="item">
+              <div href="#" id="search-sec" onClick={() => {setOpenSearch(true)}}>
+                <span className="material-symbols-outlined">search</span>
+                <label>Search</label>
+              </div>
+            </li>
+          </ul>
+          <div className="account-sec">
+            <span className="material-symbols-outlined">account_circle</span>
+          </div>
+        </nav>
+      </header>
+
+      <SearchBox show={openSearch} onClickOutside={() => {setOpenSearch(false)}}/>
+      </>
+  );
 }
 
 export default Header;
