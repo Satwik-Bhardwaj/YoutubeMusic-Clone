2a8bd6ef448f09768f5a2d123e16a953fbc641fc
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2023-06-06 19:28:17 +0530

    added the shelf and its item, corrected header and added account context menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header.js'
 import Hometab from './components/main-tab/hometab.js';
+import { AccountContextMenu } from './supercomponents/AccountContextMenu/AccountContextMenu';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Hometab/>
+      <AccountContextMenu/>
     </div>
   );
 }
