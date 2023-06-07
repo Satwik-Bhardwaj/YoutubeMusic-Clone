9b122148a121be6f04b16021679ce5b391d3692c
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2023-06-07 18:23:38 +0530

    fixed a glitch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header.js'
-import Hometab from './components/main-tab/hometab.js';
+import Hometab from './components/main-tab/Hometab.js';
 import { AccountContextMenu } from './supercomponents/AccountContextMenu/AccountContextMenu';
 
 function App() {
